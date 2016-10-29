147d779b604e9212f52ec4e6ac87d0579f25ad45
ailisp <icerove@gmail.com>
2016-10-29 18:40:26 -0400

    Update test.lisp

--- test.lisp
+++ test.lisp
@@ -1,17 +1,24 @@
-
-;;;; If tested on *slime-repl*, the output will be in *inferior-lisp*,
-;;;; which may seems not correct at first glance. But it's because the
-;;;; displace of *inferior-lisp* is not correct for exit, the actual
-;;;; function calling is correct. Try to directly tested on
-;;;; *inferior-lisp* (paste the following to *inferior-lisp* you will
-;;;; see correct result.
 (in-package :cl-user)
 (ql:quickload :exit-hooks)
 
-(exit-hooks:add-exit-hook #'(lambda () (print -2)))
-(exit-hooks:add-exit-hook #'(lambda () (print -1)))
-(exit-hooks:add-exit-hook #'(lambda () (print 1)) 1)
-(exit-hooks:add-exit-hook #'(lambda () (print 3)) 3)
-(exit-hooks:add-exit-hook #'(lambda () (print 2)) 2)
+;;; note when testing for sbcl via slime, you need to directly enter (quit) in *inferior-lisp*,
+;;; or directly use kill from shell, not *slime-repl sbcl* (which will has no effect. For ccl
+;;; and abcl there is no this issue. For ecl, I only tested directly from it's repl, because I
+;;; have problem in configuring it for slime.
+(defun save-to-file (to-save filename)
+  (with-open-file (s filename
+		     :direction :output
+		     :if-exists :append
+		     :if-does-not-exist :create)
+    (write to-save :stream s)))
+
+(exit-hooks:add-exit-hook #'(lambda () (save-to-file -2 #P"/tmp/test-exit-hooks.txt")))
+(exit-hooks:add-exit-hook #'(lambda () (save-to-file -1 #P"/tmp/test-exit-hooks.txt")) -1)
+(exit-hooks:add-exit-hook #'(lambda () (save-to-file 1 #P"/tmp/test-exit-hooks.txt")) 1)
+(exit-hooks:add-exit-hook #'(lambda () (save-to-file 3 #P"/tmp/test-exit-hooks.txt")) 3)
+(exit-hooks:add-exit-hook #'(lambda () (save-to-file 2 #P"/tmp/test-exit-hooks.txt")) 2)
 
 (exit-hooks:delete-exit-hook 2) (exit-hooks:delete-exit-hook 0)
+
+;;;; The text in /tmp/test-exit-hooks.txt should be the following after exit Lisp.
+;;;; -113
